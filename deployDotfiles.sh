3d64c40b3f0baed16519ba1acb0d33f5c8e16f97
Garrett Braatz <grbraatz@Galadriel>
2022-11-16 14:22:14 -0800

    changing from oh-my-zsh to oh-my-posh

--- deployDotfiles.sh
+++ deployDotfiles.sh
@@ -2,13 +2,10 @@
 # Make directories if not exists
 echo "Deploying Dotfiles"
 mkdir -p -- "/usr/local/bin"
-mkdir -p -- "$ZSH"
-mkdir -p -- "$ZSH/custom"
-mkdir -p -- "$ZSH/custom/themes"
-
+# 
 # Rename files if exist
 cp ~/.zshrc ~/.zshrc.predotfile 2>/dev/null
-cp "$ZSH/custom/themes/mori.zsh-theme" "$ZSH/custom/themes/mori.zsh-theme.predotfile" 2>/dev/null
+# cp "$ZSH/custom/themes/mori.zsh-theme" "$ZSH/custom/themes/mori.zsh-theme.predotfile" 2>/dev/null
 cp /usr/local/bin/qwe.sh /usr/local/bin/qwe.sh.predotfile 2>/dev/null
 
 # Link Dotfiles
@@ -16,7 +13,10 @@ cp ./system/.zshrc ~/.zshrc
 cp -u ./system/.alias ~/.alias
 cp -u ./system/.custom ~/.custom
 cp -u ./theme/dir_colors ~/.dir_colors
-cp ./theme/mori.zsh-theme "$ZSH/custom/themes/mori.zsh-theme"
+# cp ./theme/mori.zsh-theme "$ZSH/custom/themes/mori.zsh-theme"
+cp ./theme/.morioki.omp.json ~/.morioki.omp.json
 sudo ln -fn ./bin/qwe.sh /usr/local/bin/qwe.sh
 
-echo 'Restart your shell to use new dotfiles'
+chsh -s /bin/zsh
+
+echo 'Please run Compinstall and then Restart your shell to use new dotfiles'
--- installPrereqs.sh
+++ installPrereqs.sh
@@ -7,4 +7,10 @@ sudo apt-get upgrade -y
 
 sudo apt-get install zsh curl git
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+ 
+mkdir ~/.z
+cd ~/.z
+sudo wget https://raw.githubusercontent.com/rupa/z/master/z.sh
