4530463742f38e9833a2b04caaf633228a3f5d58
Garrett <morioki@protonmail.com>
2023-10-06 14:11:35 -0700

    Update installPrereqs.sh

--- installPrereqs.sh
+++ installPrereqs.sh
@@ -8,8 +8,9 @@ sudo apt-get upgrade -y
 sudo apt-get install zsh curl git
 
 # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
-sudo chmod +x /usr/local/bin/oh-my-posh
+# sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+# sudo chmod +x /usr/local/bin/oh-my-posh
+curl -s https://ohmyposh.dev/install.sh | sudo bash -s
  
 mkdir ~/.z
 cd ~/.z
